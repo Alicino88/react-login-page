4b35827447c326d1969b0bd06f07aff7431486be
Alice Moretti <ali.moret88@gmail.com>
2022-09-05 15:43:56 +0300

    Added MainHeader component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import Login from "./Components/Login/Login.js";
 import { useState } from "react";
 import Home from "./Components/Home/Home.js";
+import MainHeader from "./Components/MainHeader/MainHeader.js";
 
 function App() {
-  /*when the user enters a valid email and password (Login component) he logs in and the Home component is shown */
-  const [isLoggedIn, SetIsLoggedIn] = useState(false);
+  /*initially the Login component is shown. When the user enters a valid email and password (Login component) he logs in and the Home component is shown,
+  also the MainHeader changes*/
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const loginHandler = (email, password) => {
     console.log(email, password);
-    SetIsLoggedIn(true);
+    setIsLoggedIn(true);
   };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <main>
-      {!isLoggedIn && <Login onLogin={loginHandler} />}
-      {isLoggedIn && <Home />}
-    </main>
+    <>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home />}
+      </main>
+    </>
   );
 }
 
