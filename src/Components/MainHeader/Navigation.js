7ce8e06086e2a4b526f5355846831a272b3ae342
Alice Moretti <ali.moret88@gmail.com>
2022-09-05 15:44:12 +0300

    Added navigation component

---
+++ src/Components/MainHeader/Navigation.js
@@ -0,0 +1,27 @@
+import classes from "./Navigation.module.css";
+/*if isLoggedIn is true then the list components are shown, otherwise the ul remains empty */
+const Navigation = (props) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
