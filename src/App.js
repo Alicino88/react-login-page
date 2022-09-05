6e3e9e8826bbfa75c53668c0b0193978d2a9355b
Alice Moretti <ali.moret88@gmail.com>
2022-09-05 15:06:35 +0300

    Added Home component to show when the user is logged in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Login from "./Components/Login/Login.js";
 import { useState } from "react";
+import Home from "./Components/Home/Home.js";
 
 function App() {
+  /*when the user enters a valid email and password (Login component) he logs in and the Home component is shown */
+  const [isLoggedIn, SetIsLoggedIn] = useState(false);
   const loginHandler = (email, password) => {
     console.log(email, password);
+    SetIsLoggedIn(true);
   };
   return (
-    <div>
-      <Login onLogin={loginHandler} />
-    </div>
+    <main>
+      {!isLoggedIn && <Login onLogin={loginHandler} />}
+      {isLoggedIn && <Home />}
+    </main>
   );
 }
 
